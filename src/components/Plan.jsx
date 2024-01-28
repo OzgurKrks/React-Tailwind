ec8c06c6aadcf0bf49b6655f7157f78b17424a50
Ozgur Karakus <95243828+OzgurKrks@users.noreply.github.com>
2024-01-28 15:36:21 +0300

    test new commit

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Plan() {
   return (
     <div className="max-w-[1400px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4">
+      {/*test new branch*/}
       <div className="grid grid-cols-2 grid-rows-6 h-[80vh]">
         <img
           className="row-span-3 object-cover w-full h-full p-2"
