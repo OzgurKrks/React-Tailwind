015343ce6e3a1d456afd54bfc49a3cd798680570
Ozgur Karakus <95243828+OzgurKrks@users.noreply.github.com>
2024-02-06 22:38:55 +0300

    test

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+// test
 function Offers() {
   return (
-    <div className='max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between'>
-      <p className='text-lg font-bold text-gray-700'>Booking Incentives</p>
-      <p className='text-lg font-bold text-gray-700'>Competitive Rates</p>
-      <p className='text-lg font-bold text-gray-700'>Book With Flexibility</p>
-      <p className='text-lg font-bold text-gray-700'>Trip Insurance</p>
+    <div className="max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between">
+      <p className="text-lg font-bold text-gray-700">Booking Incentives</p>
+      <p className="text-lg font-bold text-gray-700">Competitive Rates</p>
+      <p className="text-lg font-bold text-gray-700">Book With Flexibility</p>
+      <p className="text-lg font-bold text-gray-700">Trip Insurance</p>
     </div>
   );
 }
